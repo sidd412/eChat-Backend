ed3b9d4ebc492f762a1b9c34c11ba9eaca8c2ee3
siddhartha verma <siddharthaverma6213@gmail.com>
2026-07-12 17:24:22 +0530

    Add dist to gitignore

--- dist/routes/chat.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = chatRoutes;
-const chat_controller_1 = require("../controllers/chat.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-async function chatRoutes(fastify) {
-    // Direct text chat message retrieval routes
-    fastify.get('/:chatId/messages', { preHandler: [auth_middleware_1.verifyJWT] }, chat_controller_1.getMessages);
-}
